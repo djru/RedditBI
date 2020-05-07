48b0cd806e3b381b740f8be1fbb6c842a79e2f8a
Dan Ruswick <dan.ruswick@tradingtechnologies.com>
2020-05-06 22:47:45 -0500

    cleaned out old files

--- helpers.py
+++
@@ -1,4 +0,0 @@
-import random
-import string
-def make_rand():
-    return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(12)).lower()
--- wsgi.py
+++
@@ -1,6 +0,0 @@
-from application import application
-import db
-
-if __name__ == "__main__":
-    db.create_table()
-    application.run()
